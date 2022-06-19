94cb8aa9fa0225f622557ef7237c30191cd9d211
Rita <margvlahou@gmail.com>
2022-06-19 20:11:53 +0100

    initial commit

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Box, Stack, Typography } from "@mui/material";
+
+const Footer = () => (
+  <Box mt="80px" bgcolor="#FFF3F4">
+    <Stack
+      gap="40px"
+      sx={{ alignItems: "center" }}
+      flexWrap="wrap"
+      px="40px"
+      pt="24px"
+    >
+      New Gym 2022
+    </Stack>
+  </Box>
+);
+
+export default Footer;
